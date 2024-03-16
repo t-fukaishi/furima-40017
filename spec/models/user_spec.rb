43044b3e473cc2e68d81851f4bcb79aad0110ac7
t-fukaishi <156491693+t-fukaishi@users.noreply.github.com>
2024-03-16 22:15:02 +0900

    モデルの単体テスト

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,115 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it "nicknameとemail、passwordとpassword_confirmation、birthday、last_name、first_name、last_name_kana、first_name_kanaが存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録できない場合' do
+      it "nicknameが空では登録できない" do
+         @user.nickname = ''
+         @user.valid?
+         expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "emailが空では登録できない" do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it "passwordが空では登録できない" do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = '12345'
+        @user.password_confirmation = '12345'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+
+      end
+
+      it 'passwordが129文字以上では登録できない' do
+        @user.password =  Faker::Internet.password(min_length: 129, max_length: 150)
+        @user.password_confirmation =  @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
+
+      end
+
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '1234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+
+      end
+      it '重複したemailが存在する場合は登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+
+      end
+
+      it 'emailは@を含まないと登録できない' do
+        @user.email = 'testmail'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+
+      end
+       it "birthdayが空では登録できない" do
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
+       end
+       it "last_nameが空では登録できない" do
+        @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
+        end
+        it "first_nameが空では登録できない" do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+        end
+        it "last_name_kanaが空では登録できない" do
+          @user.last_name_kana = ''
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+        end
+          it "first_name_kanaが空では登録できない" do
+            @user.first_name_kana = ''
+            @user.valid?
+            expect(@user.errors.full_messages).to include("First name kana can't be blank")
+          end
+            it 'last_nameは全角（漢字・ひらがな・カタカナ）での入力が必須であること' do
+              user = User.new(last_name: 'Test')
+              user.valid?
+              expect(user.errors[:last_name]).to include("is invalid. Input full-width characters.")
+            end
+            it 'first_nameは全角（漢字・ひらがな・カタカナ）での入力が必須であること' do
+              user = User.new(first_name: 'Test')
+              user.valid?
+              expect(user.errors[:first_name]).to include("is invalid. Input full-width characters.")
+            end
+            it 'last_name_kanaは全角（カタカナ）での入力が必須であること' do
+              user = User.new(last_name_kana: 'あいうえお')
+              user.valid?
+              expect(user.errors[:last_name_kana]).to include("is invalid. Input full-width katakana characters.")
+            end
+            it 'first_name_kanaは全角（カタカナ）での入力が必須であること' do
+              user = User.new(first_name_kana: 'あいうえお')
+              user.valid?
+              expect(user.errors[:first_name_kana]).to include("is invalid. Input full-width katakana characters.")
+            end
+    end
+  end
+end
+
