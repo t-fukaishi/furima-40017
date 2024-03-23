4235d91ca5f27d0edb75ebf28260417e6f2e6f0b
t-fukaishi <156491693+t-fukaishi@users.noreply.github.com>
2024-03-23 18:45:39 +0900

    Rubocopを実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-  @items = Item.all
+    @items = Item.all
   end
 
   def new
@@ -12,16 +12,16 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path 
+      redirect_to root_path
     else
-      render :new, status: :unprocessable_entity 
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :image, :description, :category_id, :condition_id, :delivery_fee_payment_id, :prefecture_id, :delivery_duration_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :image, :description, :category_id, :condition_id, :delivery_fee_payment_id,
+                                 :prefecture_id, :delivery_duration_id, :price).merge(user_id: current_user.id)
   end
-
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,13 +4,13 @@ FactoryBot.define do
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
-    name {'ぬいぐるみ'}
-    description {'ぬいぐるみの説明'}
-    price {400000}
-    category_id {2}
-    condition_id {2}
-    delivery_fee_payment_id {2}
-    prefecture_id {2}
-    delivery_duration_id {2}
+    name { 'ぬいぐるみ' }
+    description { 'ぬいぐるみの説明' }
+    price { 400_000 }
+    category_id { 2 }
+    condition_id { 2 }
+    delivery_fee_payment_id { 2 }
+    prefecture_id { 2 }
+    delivery_duration_id { 2 }
   end
 end
