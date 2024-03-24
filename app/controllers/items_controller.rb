36f044bca5c612aaffd7d268ee6ce0590ca3aff4
t-fukaishi <156491693+t-fukaishi@users.noreply.github.com>
2024-03-24 18:48:20 +0900

    編集画面のビューを作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,13 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+
+end
+
   #def destroy
     #item = Item.find(params[:id])
     #item.destroy
@@ -33,4 +40,3 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :image, :description, :category_id, :condition_id, :delivery_fee_payment_id,:prefecture_id, :delivery_duration_id, :price).merge(user_id: current_user.id)
   end
-end
