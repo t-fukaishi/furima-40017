1137e976eec9845cf077621b94e6d0ee662d2fcb
t-fukaishi <156491693+t-fukaishi@users.noreply.github.com>
2024-03-24 19:11:46 +0900

    updateアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
 
 end
 
