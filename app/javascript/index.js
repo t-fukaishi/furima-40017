bc300bf2c991381f5ee771ef91ac6b5a7792f82e
t-fukaishi <156491693+t-fukaishi@users.noreply.github.com>
2024-03-31 15:23:35 +0900

    イベント発火

---
+++ app/javascript/index.js
@@ -0,0 +1,9 @@
+//document.getElementById("button").addEventListener('click', function() {
+  //alert('購入ボタンがクリックされました！');
+//});
+
+const buy_Button = document.getElementById("button")
+
+buy_Button.addEventListener('click', function(){
+  console.log("購入ボタンが押されました！");
+});
