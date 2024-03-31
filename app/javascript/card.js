1ef1cf88f496ecf6290a197e7016cabc22c4f611
t-fukaishi <156491693+t-fukaishi@users.noreply.github.com>
2024-03-31 18:59:29 +0900

    フォームの情報をサーバーサイドに送信

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -17,10 +17,17 @@ const pay = () => {
       if (response.error) {
       } else {
         const token = response.id;
-        console.log(token)
+
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+
       }
+
+      document.getElementById("charge-form").submit();
+      
     });
-    
+
     e.preventDefault();
   });
 };
