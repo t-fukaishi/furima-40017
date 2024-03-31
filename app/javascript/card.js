b85b7203b94238aa9cabd379d3371be1c9deb8dc
t-fukaishi <156491693+t-fukaishi@users.noreply.github.com>
2024-03-31 18:43:26 +0900

    カード情報入力フォームのDOMからフォームに入力した値をそれぞれ取得

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,9 +1,43 @@
 const pay = () => {
+  const payjp = Payjp('pk_test_8356f34e1d3bd0c9799c57b8')// PAY.JPテスト公開鍵
+
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
   const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
-    console.log("購入ボタンが押されました！")
+
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+    
     e.preventDefault();
   });
 };
 
-window.addEventListener("turbo:load", pay);
+window.addEventListener("load", pay);
+
+
+
+
+
+
+//const pay = () => {
+  //const form = document.getElementById('charge-form')
+  //form.addEventListener("submit", (e) => {
+    //console.log("購入ボタンが押されました！")
+   // e.preventDefault();
+  //});
+//};
+
+//window.addEventListener("turbo:load", pay);
