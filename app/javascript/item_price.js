d61b28a9b4e2739c7fb779be9a329623dfe9c335
t-fukaishi <156491693+t-fukaishi@users.noreply.github.com>
2024-04-06 12:27:31 +0900

    挙動確認を修正（０４０６）

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,6 +1,6 @@
 window.addEventListener('turbo:load', () => {
-  
-  const priceInput = document.getElementById("item-price");
+
+const priceInput = document.getElementById("item-price");
 
 
   priceInput.addEventListener("input", () => {
@@ -11,8 +11,9 @@ window.addEventListener('turbo:load', () => {
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1); 
 
     const profit = document.getElementById("profit");
-    profit.innerHTML = Math.floor(inputValue * 0.9); 
+    profit.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);  
 
   
   });
 })
+
